41bbd3cb0c78782f9afc81e67d1cf345c6cd0026
Vishnu Vijayan <vvijayan@purdue.edu>
2016-12-29 17:47:48 -0500

    Now possible to toggle between light and dark themes.

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,6 +1,19 @@
 function dark() {
-  console.log('Called');
-  $('#message').css({color: "white"});
-  $('#name').css({color: "white"});
-  $('#particles-js').css({background: "#111e33"});
+  $('#message').css({color: "#d3d3d3"});
+  $('#name').css({color: "#d3d3d3"});
+  $('#particles-js').css({background: "#0e1726"});
+  $('form').css({color: "#d3d3d3"});
+  $('#chat').css({background: "rgba(204, 204, 204, 0.5)"});
+  $('#chat').css({border: "2px solid #848484", "border-radius": "2px"});
+  $('#banner').css({color: "#d3d3d3"});
+}
+
+function light() {
+  $('#message').css({color: "#232323"});
+  $('#name').css({color: "#232323"});
+  $('#particles-js').css({background: "white"});
+  $('form').css({color: "black"});
+  $('#chat').css({background: "rgba(204, 204, 204, 0.8)"});
+  $('#chat').css({border: "4px solid white", "border-radius": "2px"});
+  $('#banner').css({color: "#2d2d2d"});
 }
